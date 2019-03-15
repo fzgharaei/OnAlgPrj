f5e9449f2b0fd00ab760b74b0b938ed290e4c1b7
Fatemeh <gharaeemanesh@gmail.com>
2019-03-15 14:43:04 -0400

    fixed problem with random sizes

--- combination.cpp
+++ combination.cpp
@@ -4,32 +4,41 @@
 using namespace std;
 
 vector<int> people;
+vector< vector<int> > combinations;
 vector<int> combination;
 
-void pretty_print(const vector<int>& v) {
+void pretty_func(const vector<int>& v) {
   static int count = 0;
   cout << "combination no " << (++count) << ": [ ";
   for (int i = 0; i < v.size(); ++i) { cout << v[i] << " "; }
   cout << "] " << endl;
+	combinations.push_back(v);
 }
 
-void go(int offset, int k) {
-  if (k == 0) {
-    pretty_print(combination);
+void combination_calc(int offset, int k) {
+if (k == 0) {
+    pretty_func(combination);
     return;
   }
   for (int i = offset; i <= people.size() - k; ++i) {
     combination.push_back(people[i]);
-    go(i+1, k-1);
+    combination_calc(i+1, k-1);
     combination.pop_back();
-  }
+	}
 }
 
 int main() {
-  int n = 5, k = 3;
+  int n = 9, k = 4;
 
   for (int i = 0; i < n; ++i) { people.push_back(i+1); }
-  go(0, k);
+ 	combination_calc(0, k);
 
-  return 0;
+  for(int i =0; i<=combinations.size(); i++){
+	for(int j =0; j<combinations[i].size();j++)
+		cout<<combinations[i][j]<<" ";
+	cout<<endl;
+	cout<<"test size"<<combinations[i].size()<<endl; 
+	}
+	cout<<"combinations size"<<combinations.size()<<endl;
+return 0;
 }
