903d621ea733da0bfd3bb95334a7d7df0e42afed
Fatemeh <gharaeemanesh@gmail.com>
2019-03-15 15:06:17 -0400

    set random set

--- combination.cpp
+++ combination.cpp
@@ -28,10 +28,16 @@ if (k == 0) {
 }
 
 int main() {
-  int n = 9, k = 4;
+  int n = 6, k = 4;
 
-  for (int i = 0; i < n; ++i) { people.push_back(i); }
- 	combination_calc(0, k);
+  //for (int i = 0; i < n; ++i) { people.push_back(i); }
+	people.push_back(11);
+	people.push_back(13);
+	people.push_back(17);
+	people.push_back(19);
+	people.push_back(23);
+	people.push_back(35);
+	combination_calc(0, k);
 
   for(int i =0; i<=combinations.size(); i++){
 	for(int j =0; j<combinations[i].size();j++)
